1fea509d3767479a06e667633d0b27d47f370180
EmmanuelBett <85231611+Emmanuelbett67@users.noreply.github.com>
2022-12-22 20:05:25 +0300

    Data cleansing & Transformation

---
+++ DIM_Customers.sql
@@ -0,0 +1,38 @@
+--Cleansed DIM_CustomerTable--
+SELECT 
+  c.customerkey AS CustomerKey, 
+  --,[GeographyKey]
+  --,[CustomerAlternateKey]
+  --,[Title]
+  c.firstname AS [FirstName], 
+  --,[MiddleName]
+  c.lastname AS [LastName], 
+  c.firstname + ' ' + lastname AS [Full Name], 
+  --,[NameStyle]
+  --,[BirthDate]
+  --,[MaritalStatus]
+  --,[Suffix]
+  CASE c.gender WHEN 'M' THEN 'Male' WHEN 'F' THEN 'Female' END AS Gender, 
+  --,[EmailAddress]
+  --,[YearlyIncome]
+  --,[TotalChildren]
+  --,[NumberChildrenAtHome]
+  --,[EnglishEducation]
+  --,[SpanishEducation]
+  --,[FrenchEducation]
+  --,[EnglishOccupation]
+  --,[SpanishOccupation]
+  --,[FrenchOccupation]
+  --,[HouseOwnerFlag]
+  --,[NumberCarsOwned]
+  --,[AddressLine1]
+  --,[AddressLine2]
+  --,[Phone]
+  c.datefirstpurchase AS DateFirstPurchase, 
+  --,[CommuteDistance]
+  g.city AS [Customer City] --joined Customer City from Geography Table
+FROM 
+  dbo.dimcustomer AS c 
+  LEFT JOIN dbo.dimgeography AS g ON g.geographykey = c.geographykey 
+ORDER BY 
+  CustomerKey ASC --ordered the list by CustomerKey
